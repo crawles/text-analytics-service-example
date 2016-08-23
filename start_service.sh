81b4ee3a319d4ce9526c8284d9005fb38faa9516
Chris Rawles <crawles@gmail.com>
2016-08-23 13:36:15 -0400

    updated notebook

--- start_service.sh
+++ start_service.sh
@@ -1,5 +1,4 @@
-unset PYTHONPATH
 jupyter-kernelgateway --KernelGatewayApp.port=$PORT\
                       --KernelGatewayApp.ip=0.0.0.0\
                       --KernelGatewayApp.api=notebook-http\
-                      --KernelGatewayApp.seed_uri='Untitled.ipynb'
+                      --KernelGatewayApp.seed_uri='https://raw.githubusercontent.com/crawles/text-analytics-service-example/master/Untitled.ipynb'
