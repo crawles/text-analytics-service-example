c26f37c7978c7cf33f5a1240e945ea1b28497c78
Chris Rawles <crawles@gmail.com>
2016-08-24 14:08:14 -0400

    .

--- start_service.sh
+++
@@ -1,4 +0,0 @@
-jupyter-kernelgateway --KernelGatewayApp.port=8080\
-                      --KernelGatewayApp.ip=0.0.0.0\
-                      --KernelGatewayApp.api=notebook-http\
-                      --KernelGatewayApp.seed_uri='https://raw.githubusercontent.com/crawles/text-analytics-service-example/master/Untitled.ipynb'
