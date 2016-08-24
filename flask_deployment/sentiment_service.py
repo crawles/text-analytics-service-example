2bdc7a695459dc67877bf8ccd7f38ba238af32b5
Chris Rawles <crawles@gmail.com>
2016-08-24 10:23:04 -0400

    Fixed environment, changed manifest to 1GB

--- flask_deployment/sentiment_service.py
+++ flask_deployment/sentiment_service.py
@@ -1,7 +1,17 @@
-from flask import Flask, request
+# -*- coding: utf-8 -*-
+
+'''
+sentiment_service.py
+~~~~~~~~~~~~~~~~~~~~
+
+App implements a pre-trained sentiment analysis pipeline. 
+
+'''
+import os
 import urllib
 
 import dill
+from flask import Flask, request, jsonify
 import pandas as pd
 
 f = urllib.request.urlopen("https://github.com/crawles/sentiment_analysis_twitter_model/raw/master/twitter_sentiment_model.pkl")
@@ -9,11 +19,19 @@ cl = dill.load(f)
 f.close()
 
 app = Flask(__name__)
-@app.route('/identify', methods=['POST'])
+
+@app.route('/polarity_compute', methods=['POST'])
 def sentiment_compute():
     req = request.get_json(force=True)
-    print(req)
     prediction = cl.predict_proba(req['data'])[:][:,1]
-    return(str(prediction))
+    return(jsonify({"polarity" : prediction.tolist()}))
 
-app.run(debug = True)
+if __name__ == "__main__":
+    if os.environ.get('VCAP_SERVICES') is None: # running locally
+        PORT = 8080
+        DEBUG = True
+    else:                                       # running on CF
+        PORT = int(os.getenv("PORT"))
+        DEBUG = False
+
+    app.run(host='0.0.0.0', port=PORT, debug=DEBUG)
